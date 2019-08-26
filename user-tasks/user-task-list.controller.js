ff42ade85b4f6dd36f4d51106b470448a1e30b78
Dzmitry Lanhe <Dzmitry_Lanhe@epam.com>
2019-08-26 17:52:36 +0300

    add langs content

--- user-tasks/user-task-list.controller.js
+++ user-tasks/user-task-list.controller.js
@@ -6,7 +6,12 @@
 		.controller("UserTaskList", UserTaskList);
 
 	function UserTaskList() {
-		
+		$scope.langState = 'en';
+
+
+		function changeLangState(langCode) {
+			$scope.langState = langCode;
+		}
 	}
 
 })();

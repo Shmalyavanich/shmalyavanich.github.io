bb02fe98529ff059862272759e5b1177d015ada4
Dzmitry Lanhe <Dzmitry_Lanhe@epam.com>
2019-08-26 18:17:27 +0300

    fix langs functionality

--- tasks/task-list/task-list.controller.js
+++ tasks/task-list/task-list.controller.js
@@ -8,13 +8,22 @@
 	TaskList.$inject = ["tasksSrv"];
 	
 	function TaskList(tasksSrv) {
+		$scope.changeLangState = changeLangState;
+
 		console.log('controller');
+
 		let $ctrl = this;
 
 		init();
 
 		function init() {
 			$ctrl.tasks = tasksSrv.getAllTasks()
+		}
+
+		$ctrl.langState = 'en';
+
+		function changeLangState(langCode) {
+			$ctrl.langState = langCode;
 		}
 	}
 

0117661087e2ad63ab0221b7dfc30f53dac21ecc
Dzmitry Lanhe <Dzmitry_Lanhe@epam.com>
2019-08-27 11:52:53 +0300

    fix langs functionality

--- users/user-list.controller.js
+++ users/user-list.controller.js
@@ -18,31 +18,30 @@
 			usersSrv.getData().then( function (data) {$ctrl.users = data} );
 		}
 
+		var elementEn = document.createElement("link");
+		elementEn.setAttribute("rel", "alternate");
+		elementEn.setAttribute("hreflang", "en");
+		elementEn.setAttribute("href", "https://shmalyavanich.github.io/#!/users");
+
+		var elementRu = document.createElement("link");
+		elementEn.setAttribute("rel", "alternate");
+		elementEn.setAttribute("hreflang", "ru");
+		elementEn.setAttribute("href", "https://shmalyavanich.github.io/#!/users?hl=ru");
+
+		var elementUa = document.createElement("link");
+		elementEn.setAttribute("rel", "alternate");
+		elementEn.setAttribute("hreflang", "ua");
+		elementEn.setAttribute("href", "https://shmalyavanich.github.io/#!/users?hl=ua");
+
+		var head = document.getElementByName("head");
+		head.append(elementEn);
+		head.append(elementRu);
+		head.append(elementUa);
 
     $ctrl.langState = 'en';
 
     function changeLangState(langCode) {
       $ctrl.langState = langCode;
-
-      var elementEn = document.createElement("link");
-			elementEn.setAttribute("rel", "alternate");
-			elementEn.setAttribute("hreflang", "en");
-			elementEn.setAttribute("href", "https://shmalyavanich.github.io/#!/users");
-
-			var elementRu = document.createElement("link");
-			elementEn.setAttribute("rel", "alternate");
-			elementEn.setAttribute("hreflang", "ru");
-			elementEn.setAttribute("href", "https://shmalyavanich.github.io/#!/users?hl=ru");
-
-			var elementUa = document.createElement("link");
-			elementEn.setAttribute("rel", "alternate");
-			elementEn.setAttribute("hreflang", "ua");
-			elementEn.setAttribute("href", "https://shmalyavanich.github.io/#!/users?hl=ua");
-
-			var head = document.getElementByName("head");
-			head.append(elementEn);
-			head.append(elementRu);
-			head.append(elementUa);
 
 			switch (langCode) {
 				case 'en':
